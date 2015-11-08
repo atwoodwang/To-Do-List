9392f7acb8e33cfa5b1c71484435e42c2ed6aa1d
NoAnyLove <noanylove@gmail.com>
2015-11-08 03:58:03 -0500

    Add TaskCursorWrapper class

---
+++ app/src/main/java/edu/osu/cse/todolist/to_dolist/database/TaskCursorWrapper.java
@@ -0,0 +1,57 @@
+package edu.osu.cse.todolist.to_dolist.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import edu.osu.cse.todolist.to_dolist.Location;
+import edu.osu.cse.todolist.to_dolist.Task;
+import edu.osu.cse.todolist.to_dolist.Task.ConfigType;
+
+import static edu.osu.cse.todolist.to_dolist.database.ToDoDbSchema.*;
+
+/**
+ * Created by Sniper on 2015/11/8.
+ */
+public class TaskCursorWrapper extends CursorWrapper {
+    /**
+     * Creates a cursor wrapper.
+     *
+     * @param cursor The underlying cursor to wrap.
+     */
+    public TaskCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Task get() {
+        // get field values
+        long id = getLong((getColumnIndex(TaskTable.Cols.ID)));
+        String title = getString((getColumnIndex(TaskTable.Cols.TITLE)));
+        String note = getString((getColumnIndex(TaskTable.Cols.NOTE)));
+        boolean starred = getInt(getColumnIndex(TaskTable.Cols.STARRED)) != 0;
+        int configInt = getInt(getColumnIndex(TaskTable.Cols.CONFIG));
+        ConfigType config = ConfigType.values()[configInt];
+        boolean complete = getInt(getColumnIndex(TaskTable.Cols.COMPLETE)) != 0;
+
+        // create Task object and set values
+        Task task = new Task(id);
+        task.setTitle(title);
+        task.setNote(note);
+        task.setStarred(starred);
+        task.setConfig(config);
+        task.setComplete(complete);
+
+        // TODO: set Location and Schedule
+        switch (config) {
+            case NONE:
+                break;
+            case TIME:
+                break;
+            case LOCATION_ARRIVING:
+                break;
+            case LOCATION_LEAVING:
+                break;
+        }
+
+        return task;
+    }
+}
